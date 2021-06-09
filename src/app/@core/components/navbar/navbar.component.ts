8d8d4b731c300f556eb97d890aebe66e1cc807cf
Anartz Mugika Ledo <mugan86@gmail.com>
2021-06-09 12:21:09 +0200

    Reactive Form apply with basic config and implementation

--- src/app/@core/components/navbar/navbar.component.ts
+++ src/app/@core/components/navbar/navbar.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { GENERAL } from '@core/constants/general-configs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  title=GENERAL.title
+}
